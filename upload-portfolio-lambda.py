334d0fb9a003cd365ed88fa8f68e319584ff877e
Juha Fagerstedt <sashadb16@gmail.com>
2018-01-20 22:39:02 +0200

    SNS notification when deploy

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,21 +4,29 @@ from io import BytesIO
 import mimetypes
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:127458414699:deployDilleDalleSiteTopic')
 
-    portfolio_bucket = s3.Bucket('www.dilledalle.com')
-    build_bucket = s3.Bucket('www.dilledalle.com.build')
+    try:
+        s3 = boto3.resource('s3')
 
-    portfolio_zip = BytesIO()
-    build_bucket.download_fileobj('dilledallebuild.zip', portfolio_zip)
+        portfolio_bucket = s3.Bucket('www.dilledalle.com')
+        build_bucket = s3.Bucket('www.dilledalle.com.build')
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        portfolio_zip = BytesIO()
+        build_bucket.download_fileobj('dilledallebuild.zip', portfolio_zip)
 
-    print "Job Done Sasha"
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+        print "Job Done Sasha"
+        topic.publish(Subject="www.dilledalle.com deployed", Message="www.dilledalle.com deployed succesfully")
+    except:
+        topic.publish(Subject="www.dilledalle.com deploy failed", Message="www.dilledalle.com was not deployed succesfully")
+        raise
 
     return 'Hello from Lambda'
