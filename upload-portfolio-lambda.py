29cd3ac674c7b0207e53176fbf2c734a632949ac
Juha Fagerstedt <sashadb16@gmail.com>
2018-01-20 09:04:02 +0200

    Update portfolio update function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -3,17 +3,22 @@ import zipfile
 from io import BytesIO
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('www.dilledalle.com')
-build_bucket = s3.Bucket('www.dilledalle.com.build')
+    portfolio_bucket = s3.Bucket('www.dilledalle.com')
+    build_bucket = s3.Bucket('www.dilledalle.com.build')
 
-portfolio_zip = BytesIO()
-build_bucket.download_fileobj('dilledallebuild.zip', portfolio_zip)
+    portfolio_zip = BytesIO()
+    build_bucket.download_fileobj('dilledallebuild.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Job Done Sasha"
+
+    return 'Hello from Lambda'
